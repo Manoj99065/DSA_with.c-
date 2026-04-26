433b546ad365b52f17e666599e861badb27e4221
Manoj Thakur <manoj99065@gmail.com>
2026-04-26 16:46:17 +0530

    Time: 2 ms (3.14%) | Memory: 18.8 MB (5.43%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -9,29 +9,70 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+
+// bool mirror(TreeNode* leftptr ,TreeNode * rightptr)
+// {
+//         if(leftptr==nullptr  && rightptr==nullptr)
+//         {
+//             return true;
+//         }
+
+//         if(leftptr==nullptr || rightptr==nullptr || leftptr->val !=rightptr->val)
+//         {
+//             return false;
+//         }
+//         return mirror(leftptr->left , rightptr->right) && mirror(leftptr->right,rightptr->left);
+        
+//     }
+//     bool isSymmetric(TreeNode* root) {
+//         if(root==nullptr)
+//         {
+//             return true;
+//         }
+
+//         return mirror(root->left,root->right);
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
+bool isSymmetric(TreeNode* root) {
 
-bool mirror(TreeNode* leftptr ,TreeNode * rightptr)
-{
-        if(leftptr==nullptr  && rightptr==nullptr)
+    if(root==nullptr)
+    {
+        return true;
+    }
+    stack<TreeNode*>s1,s2;
+    s1.push(root->left);
+    s2.push(root->right);
+    while(!s1.empty() && !s2.empty())
+    {
+        TreeNode* node1=s1.top();
+        TreeNode* node2=s2.top();
+        s1.pop();
+        s2.pop();
+
+        if(node1==nullptr && node2==nullptr)
         {
-            return true;
+            continue;
         }
-
-        if(leftptr==nullptr || rightptr==nullptr || leftptr->val !=rightptr->val)
+        if(node1==nullptr || node2==nullptr || node1->val != node2->val)
         {
             return false;
         }
-        return mirror(leftptr->left , rightptr->right) && mirror(leftptr->right,rightptr->left);
-        
-    }
-    bool isSymmetric(TreeNode* root) {
-        if(root==nullptr)
-        {
-            return true;
-        }
 
-        return mirror(root->left,root->right);
+        s1.push(node1->left);
+        s2.push(node2->right);
+
+        s1.push(node1->right);
+        s2.push(node2->left);
+    }
+    return s1.empty() && s2.empty();
     }
 };
