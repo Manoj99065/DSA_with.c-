6ad0cab2bf53cc648c3ff79265345533ff43df25
Manoj Thakur <manoj99065@gmail.com>
2026-04-28 18:21:02 +0530

    Time: 1 ms (4.10%) | Memory: 19.2 MB (13.32%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -1,25 +1,52 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     int maxDepth(TreeNode * root)
+//     {
+//         if(root==nullptr)
+//         {
+//             return 0 ;
+//         }
+//         int left=maxDepth(root->left);
+//         int right=maxDepth(root->right);
+//         int ans=max(right,left)+1;
+//         return ans;   
+//     }
+// };
+
+
+
 class Solution {
 public:
     int maxDepth(TreeNode * root)
     {
-        if(root==nullptr)
+        if(root==nullptr)return 0;
+        queue<TreeNode*> qu;
+        qu.push(root);
+        int depth=0;
+        while(!qu.empty())
         {
-            return 0 ;
+            int size=qu.size();
+           
+            for(int i=0;i<size;i++)
+            {
+                TreeNode* node =qu.front();
+                qu.pop();
+             if(node->left) qu.push(node->left);
+             if(node->right)qu.push(node->right);
+            }
+        depth++;
         }
-        int left=maxDepth(root->left);
-        int right=maxDepth(root->right);
-        int ans=max(right,left)+1;
-        return ans;   
+        return depth;
     }
 };
