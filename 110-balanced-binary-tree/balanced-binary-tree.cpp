d01bc9640f0dcaac3a5ea6acb7344e34a6ec8888
Manoj Thakur <manoj99065@gmail.com>
2026-04-29 10:46:24 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (53.98%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -1,32 +1,72 @@
 
+// class Solution {
+//     private:
+//     int height(TreeNode* root)
+//     {
+//         if(root==nullptr)
+//         {
+//             return true;
+//         }
+//         int op1=height(root->left);
+//         int op2=height(root->right);
+//         int ans(max(op1,op2)+1);
+//         return ans;
+//     }
+
+
+// public:
+//     bool isBalanced(TreeNode* root) {
+//         if(root==nullptr)
+//         {
+//             return true;
+//         }
+//             bool left=isBalanced(root->left);
+//             bool right=isBalanced(root->right);
+//             bool ans= abs(height(root->left)-height(root->right))<=1;
+//             if(left && right && ans)
+//             {
+//                 return true;
+//             }    
+//         return false;   
+//     }
+// };
+
+
 class Solution {
     private:
-    int height(TreeNode* root)
+    pair<bool,int>isBalanc(TreeNode * root)
     {
         if(root==nullptr)
         {
-            return true;
+            pair<bool,int>p=make_pair(true,0);
+            return p;
         }
-        int op1=height(root->left);
-        int op2=height(root->right);
-        int ans(max(op1,op2)+1);
+
+        pair<bool,int>left=isBalanc(root->left);
+        pair<bool,int>right=isBalanc(root->right);
+
+        bool leftans=left.first;
+        bool rightans=right.first;
+        bool diff=(abs(left.second-right.second))<=1;
+
+
+        pair<bool,int>ans;
+        ans.second=max(left.second,right.second)+1;
+
+        if(leftans && rightans && diff)
+        {
+            ans.first=true;
+        }
+        else
+        {
+            ans.first=false;
+        }
         return ans;
     }
 
 
-public:
+    public:
     bool isBalanced(TreeNode* root) {
-        if(root==nullptr)
-        {
-            return true;
-        }
-            bool left=isBalanced(root->left);
-            bool right=isBalanced(root->right);
-            bool ans= abs(height(root->left)-height(root->right))<=1;
-            if(left && right && ans)
-            {
-                return true;
-            }    
-        return false;   
+       return isBalanc(root).first;
     }
 };
