80508589dd515a1ec90194e715a1a6feadad39ef
Manoj Thakur <manoj99065@gmail.com>
2026-05-18 01:12:04 +0530

    Time: 7 ms (97.33%) | Memory: 25 MB (98.96%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -0,0 +1,24 @@
+# class Solution:
+#     def canReach(self, arr: List[int], start: int) -> bool:
+        
+
+
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        n = len(arr)
+        visited = [False] * n
+        q = deque([start])
+        visited[start] = True
+
+        while q:
+            i = q.popleft()
+
+            if arr[i] == 0:
+                return True
+
+            for nxt in (i + arr[i], i - arr[i]):
+                if 0 <= nxt < n and not visited[nxt]:
+                    visited[nxt] = True
+                    q.append(nxt)
+
+        return False
