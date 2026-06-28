684243db63be4e89deb50b0456117271f6b0f9fe
Manoj Thakur <manoj99065@gmail.com>
2026-06-28 13:09:35 +0530

    Time: 7 ms (74.58%) | Memory: 55.2 MB (69.21%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -1,42 +1,45 @@
-// class Solution {
-// public:
-//     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-//         sort(arr.begin(),arr.end());
-//         if(arr[0]!=1)
-//         {
-//             arr[0]=1;
-//         }
-//         long long  i=1;
-//         while(i<arr.size())
-//         {
-//             if(arr[i]-arr[i-1]<=1)
-//             {
-//               i++;
-//             }
-//             else
-//             {
-//                 arr[i]=arr[i-1]+1;
-//             }
-//         }
-//         return *max_element(arr.begin(),arr.end());
-//     }
-// };
-
-
 class Solution {
 public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> freq(n + 1,0);
-        for(int i =0;i<n;i++){
-            freq[min(arr[i],n)]++;
+        sort(arr.begin(),arr.end());
+        if(arr[0]!=1)
+        {
+            arr[0]=1;
         }
-        int ans = 0;
-        for(int i =1;i<=n;i++){
-            if(freq[i] == 0) continue;
-
-            ans = min(ans + freq[i],i);
+        int ans=1;
+        long long  i=1;
+        while(i<arr.size())
+        {
+            if(arr[i]-arr[i-1]<=1)
+            {
+              i++;
+            }
+            else
+            {
+                arr[i]=arr[i-1]+1;
+            }
+            ans=max(ans,arr[i-1]);
         }
+        // return *max_element(arr.begin(),arr.end());
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+//         int n = arr.size();
+//         vector<int> freq(n + 1,0);
+//         for(int i =0;i<n;i++){
+//             freq[min(arr[i],n)]++;
+//         }
+//         int ans = 0;
+//         for(int i =1;i<=n;i++){
+//             if(freq[i] == 0) continue;
+
+//             ans = min(ans + freq[i],i);
+//         }
+//         return ans;
+//     }
+// };
