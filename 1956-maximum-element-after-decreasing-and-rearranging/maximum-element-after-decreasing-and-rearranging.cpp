a1ffa7ee7ed14ca5b0cba9af8611fea5df4aa474
Manoj Thakur <manoj99065@gmail.com>
2026-06-28 11:58:22 +0530

    Time: 3 ms (94.63%) | Memory: 57.5 MB (4.52%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -1,23 +1,42 @@
+// class Solution {
+// public:
+//     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+//         sort(arr.begin(),arr.end());
+//         if(arr[0]!=1)
+//         {
+//             arr[0]=1;
+//         }
+//         long long  i=1;
+//         while(i<arr.size())
+//         {
+//             if(arr[i]-arr[i-1]<=1)
+//             {
+//               i++;
+//             }
+//             else
+//             {
+//                 arr[i]=arr[i-1]+1;
+//             }
+//         }
+//         return *max_element(arr.begin(),arr.end());
+//     }
+// };
+
+
 class Solution {
 public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
-        if(arr[0]!=1)
-        {
-            arr[0]=1;
+        int n = arr.size();
+        vector<int> freq(n + 1,0);
+        for(int i =0;i<n;i++){
+            freq[min(arr[i],n)]++;
         }
-        long long  i=1;
-        while(i<arr.size())
-        {
-            if(arr[i]-arr[i-1]<=1)
-            {
-              i++;
-            }
-            else
-            {
-                arr[i]=arr[i-1]+1;
-            }
+        int ans = 0;
+        for(int i =1;i<=n;i++){
+            if(freq[i] == 0) continue;
+
+            ans = min(ans + freq[i],i);
         }
-        return *max_element(arr.begin(),arr.end());
+        return ans;
     }
 };
