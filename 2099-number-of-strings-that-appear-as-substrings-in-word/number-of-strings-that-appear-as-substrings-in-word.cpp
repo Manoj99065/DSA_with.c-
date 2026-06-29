b9ad93c614af7ec18823124cdad94e2c3a78bfdd
Manoj Thakur <manoj99065@gmail.com>
2026-06-29 09:27:02 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (19.66%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,20 +1,55 @@
+// class Solution {
+// public:
+//     int numOfStrings(vector<string>& patterns, string word) {
+//         int size=patterns.size();
+//         int count=0;
+//         for(int i=0;i<size;i++)
+//         {
+//            if(word.contains(patterns[i]))
+//             {
+//                 count++;
+//             }
+//             else
+//             {
+//                 continue;
+//             }
+//         } 
+//         return count;       
+//     }
+// };
+
 class Solution {
 public:
     int numOfStrings(vector<string>& patterns, string word) {
-        int size=patterns.size();
-        // cout<<size;
-        int count=0;
-        for(int i=0;i<size;i++)
-        {
-           if(word.contains(patterns[i]))
-            {
+        int n = patterns.size();
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+            string pat = patterns[i];
+            int m = word.size();
+            int k = pat.size();
+
+            bool found = false;
+
+            // manual substring check
+            for(int j = 0; j <= m - k; j++) {
+                int l = 0;
+
+                while(l < k && word[j + l] == pat[l]) {
+                    l++;
+                }
+
+                if(l == k) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if(found) {
                 count++;
             }
-            else
-            {
-                continue;
-            }
-        } 
-        return count;       
+        }
+
+        return count;
     }
 };
