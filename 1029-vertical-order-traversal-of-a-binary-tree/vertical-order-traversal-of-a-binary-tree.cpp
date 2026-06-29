a13eac16499814f7245d9c38b7791173953c75bc
Manoj Thakur <manoj99065@gmail.com>
2026-06-29 10:59:31 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (72.27%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,39 +12,40 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        
-        map<int , map<int, multiset<int>>> nodes;
-        queue<pair<TreeNode*, pair<int,int>>> qu;
-        vector<vector<int>> ans;
-        if(root == NULL) return ans;
+        map<int,map<int,vector<int>>>nodes;
+        queue<pair<TreeNode*,pair<int,int>>>qu;
+        vector<vector<int>>ans;
+        if(root==NULL)
+        {
+            return ans;
+        }
 
         qu.push(make_pair(root,make_pair(0,0)));
-
         while(!qu.empty())
         {
-            pair<TreeNode*,pair<int,int>> temp=qu.front();
+            pair<TreeNode*,pair<int,int>>temp=qu.front();
             qu.pop();
-            TreeNode* frontNode=temp.first;
+            TreeNode * firstNode=temp.first;
             int hd=temp.second.first;
-            int lvl=temp.second.second;
+            int level=temp.second.second;
+            nodes[hd][level].push_back(firstNode->val);
 
-
-            nodes[hd][lvl].insert(frontNode->val);
-            if(frontNode->left)
+            if(firstNode->left)
             {
-                qu.push(make_pair(frontNode->left,make_pair(hd-1,lvl+1)));
+                qu.push(make_pair(firstNode->left,make_pair(hd-1,level+1)));
             }
-            if(frontNode->right)
+             if(firstNode->right)
             {
-                qu.push(make_pair(frontNode->right,make_pair(hd+1,lvl+1)));
+                qu.push(make_pair(firstNode->right,make_pair(hd+1,level+1)));
             }
         }
 
-        for(auto i: nodes)
+         for(auto i:nodes)
         {
             vector<int>col;
-            for(auto j: i.second)
+            for(auto j:i.second)
             {
+                sort(j.second.begin(), j.second.end());
                 for(auto k:j.second)
                 {
                     col.push_back(k);
@@ -52,6 +53,48 @@ public:
             }
             ans.push_back(col);
         }
-        return ans; 
+        
+    return ans;  
     }
 };
+
+//  map<int , map<int, multiset<int>>> nodes;
+//         queue<pair<TreeNode*, pair<int,int>>> qu;
+//         vector<vector<int>> ans;
+//         if(root == NULL) return ans;
+
+//         qu.push(make_pair(root,make_pair(0,0)));
+
+//         while(!qu.empty())
+//         {
+//             pair<TreeNode*,pair<int,int>> temp=qu.front();
+//             qu.pop();
+//             TreeNode* frontNode=temp.first;
+//             int hd=temp.second.first;
+//             int lvl=temp.second.second;
+
+
+//             nodes[hd][lvl].insert(frontNode->val);
+//             if(frontNode->left)
+//             {
+//                 qu.push(make_pair(frontNode->left,make_pair(hd-1,lvl+1)));
+//             }
+//             if(frontNode->right)
+//             {
+//                 qu.push(make_pair(frontNode->right,make_pair(hd+1,lvl+1)));
+//             }
+//         }
+
+//         for(auto i: nodes)
+//         {
+//             vector<int>col;
+//             for(auto j: i.second)
+//             {
+//                 for(auto k:j.second)
+//                 {
+//                     col.push_back(k);
+//                 }
+//             }
+//             ans.push_back(col);
+//         }
+//         return ans; 
