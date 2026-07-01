e503bf15c24bf6960b8b178c9daaaaf293f8629c
Manoj Thakur <manoj99065@gmail.com>
2026-07-01 17:59:02 +0530

    Time: 67 ms (21.99%) | Memory: 51 MB (5.16%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -46,17 +46,53 @@
 //     }
 // };
 
-class Solution {
-public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root || root == p || root == q)
-            return root;
+// class Solution {
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if (!root || root == p || root == q)
+//             return root;
         
-        TreeNode* lca_from_left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* lca_from_right = lowestCommonAncestor(root->right, p, q);
-        if (lca_from_left && lca_from_right)
-            return root;
+//         TreeNode* lca_from_left = lowestCommonAncestor(root->left, p, q);
+//         TreeNode* lca_from_right = lowestCommonAncestor(root->right, p, q);
+//         if (lca_from_left && lca_from_right)
+//             return root;
 
-        return lca_from_left ? lca_from_left : lca_from_right;
+//         return lca_from_left ? lca_from_left : lca_from_right;
+//     }
+// };
+
+class Solution
+{
+    public:
+
+bool findPath(TreeNode* root, vector<TreeNode*>& path, TreeNode * n) {
+    if (root == nullptr) return false;
+
+    // Store current node
+    path.push_back(root);
+
+    if (root == n || findPath(root->left, path, n) || findPath(root->right, path, n)) {
+        return true;
     }
+
+    // remove root from path and return false
+    path.pop_back();
+    return false;
+}
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* n1, TreeNode* n2) {
+    vector<TreeNode*> path1, path2;
+
+    // Find paths from root to n1 
+    // and root to n2
+    if (!findPath(root, path1, n1) || !findPath(root, path2, n2)) return nullptr;
+
+    // Compare the paths to get 
+    // the first different value
+    int i = 0;
+    for (i = 0; i < path1.size() && i < path2.size(); i++) {
+        if (path1[i] != path2[i]) return path1[i - 1];
+    }
+    return path1[i - 1];
+}
 };
