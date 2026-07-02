844d6d8c80be1476dcccfa249869eb43d7a7d72c
Manoj Thakur <manoj99065@gmail.com>
2026-07-02 23:51:00 +0530

    Time: 14 ms (74.11%) | Memory: 33.7 MB (38.98%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -1,50 +1,95 @@
+// // class Solution {
+// // public:
+// //     bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        
+// //     }
+// // };
+
 // class Solution {
 // public:
 //     bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        
+//         int m = grid.size(), n = grid[0].size();
+
+//         vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+
+//         deque<pair<int,int>> dq;
+
+//         dist[0][0] = grid[0][0];
+//         dq.push_front({0,0});
+
+//         vector<pair<int,int>> dirs = {
+//             {1,0}, {-1,0}, {0,1}, {0,-1}
+//         };
+
+//         while (!dq.empty()) {
+//             auto [r, c] = dq.front();
+//             dq.pop_front();
+
+//             for (auto &[dr, dc] : dirs) {
+//                 int nr = r + dr;
+//                 int nc = c + dc;
+
+//                 if (nr < 0 || nr >= m || nc < 0 || nc >= n)
+//                     continue;
+
+//                 int cost = grid[nr][nc];
+
+//                 if (dist[r][c] + cost < dist[nr][nc]) {
+//                     dist[nr][nc] = dist[r][c] + cost;
+
+//                     if (cost == 0)
+//                         dq.push_front({nr, nc});
+//                     else
+//                         dq.push_back({nr, nc});
+//                 }
+//             }
+//         }
+
+//         return dist[m-1][n-1] < health;
 //     }
 // };
 
 class Solution {
-public:
+public: 
+    vector<vector<int>>direction{{0,1},{0,-1},{1,0},{-1,0}} ;
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        int m = grid.size(), n = grid[0].size();
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>>result(m,vector<int>(n,INT_MAX));
+        deque<pair<int,int>>deq;
+        result[0][0]=grid[0][0];
 
-        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        deq.push_front({0,0});
+        while(!deq.empty())
+        {
+            auto[r,c]=deq.front();
+            deq.pop_front();
 
-        deque<pair<int,int>> dq;
+            for(auto & dir:direction)
+            {
+                       int nr=r+dir[0];
+                       int nc=c+dir[1];
 
-        dist[0][0] = grid[0][0];
-        dq.push_front({0,0});
+                       if(nr<0 || nr >=m || nc<0 || nc>=n)continue;
 
-        vector<pair<int,int>> dirs = {
-            {1,0}, {-1,0}, {0,1}, {0,-1}
-        };
+                       if(result[r][c]+grid[nr][nc]<result[nr][nc])
+                       {
+                        result[nr][nc]=result[r][c]+grid[nr][nc];
 
-        while (!dq.empty()) {
-            auto [r, c] = dq.front();
-            dq.pop_front();
 
-            for (auto &[dr, dc] : dirs) {
-                int nr = r + dr;
-                int nc = c + dc;
-
-                if (nr < 0 || nr >= m || nc < 0 || nc >= n)
-                    continue;
-
-                int cost = grid[nr][nc];
-
-                if (dist[r][c] + cost < dist[nr][nc]) {
-                    dist[nr][nc] = dist[r][c] + cost;
-
-                    if (cost == 0)
-                        dq.push_front({nr, nc});
-                    else
-                        dq.push_back({nr, nc});
-                }
+                        if(grid[nr][nc]==0)
+                        {
+                            deq.push_front({nr,nc});
+                        }
+                        else
+                        {
+                            deq.push_back({nr,nc});
+                        }
+                       }
             }
         }
+        int x=result[m-1][n-1];
+        return health-x>=1;
 
-        return dist[m-1][n-1] < health;
     }
 };
