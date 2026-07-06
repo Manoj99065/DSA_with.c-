ba26983c3b28e6c6e5a3049bf2b5b18e2345d18a
Manoj Thakur <manoj99065@gmail.com>
2026-07-07 00:09:50 +0530

    Time: 4 ms (28.48%) | Memory: 15.2 MB (60.13%) - LeetSync

---
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -0,0 +1,31 @@
+// class Solution {
+// public:
+//     int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        
+//     }
+// };
+
+
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 if (a[0] == b[0])
+                     return a[1] > b[1];   // larger end first
+                 return a[0] < b[0];       // smaller start first
+             });
+
+        int ans = 0;
+        int maxEnd = -1;
+
+        for (auto &interval : intervals) {
+            if (interval[1] > maxEnd) {
+                ans++;
+                maxEnd = interval[1];
+            }
+        }
+
+        return ans;
+    }
+};
