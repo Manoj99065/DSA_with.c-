e9c459736a7d7662d514eeca7090679c5209bd23
Manoj Thakur <manoj99065@gmail.com>
2026-07-07 02:25:35 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (81.95%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -1,31 +1,31 @@
-// class Solution {
-// public:
-//     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        
-//     }
-// };
-
-
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(),
-             [](const vector<int>& a, const vector<int>& b) {
-                 if (a[0] == b[0])
-                     return a[1] > b[1];   // larger end first
-                 return a[0] < b[0];       // smaller start first
-             });
-
-        int ans = 0;
-        int maxEnd = -1;
-
-        for (auto &interval : intervals) {
-            if (interval[1] > maxEnd) {
-                ans++;
-                maxEnd = interval[1];
+        vector<vector<int>>result;
+        int n=intervals.size();
+        
+        auto lambda=[](vector<int>&vec1, vector<int> & vec2)
+        {
+            if(vec1[0]==vec2[0])
+            {
+                return vec1[1]>vec2[1];
             }
-        }
+            return vec1[0]<vec2[0];
+        };
 
-        return ans;
+        sort(begin(intervals),end(intervals),lambda);
+        
+
+        result.push_back(intervals[0]);
+        for(int i=0;i<n;i++)
+        {
+            if(result.back()[0]<=intervals[i][0] && result.back()[1]>=intervals[i][1])
+            {
+                continue;
+            }
+
+            result.push_back(intervals[i]);
+        }
+        return result.size();
     }
 };
