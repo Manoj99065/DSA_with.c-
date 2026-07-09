e17eed4244c8418e607cd4ea194b2e96fd60a4f8
Manoj Thakur <manoj99065@gmail.com>
2026-07-09 11:21:39 +0530

    Time: 39 ms (70.78%) | Memory: 155.3 MB (60.57%) - LeetSync

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -1,119 +1,182 @@
+// // class Solution {
+// // public:
+// //     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+// //         const long long MOD = 1e9 + 7;
+// //         vector<int>ans;
+// //         int size=queries.size();
+// //         for(int i=0;i<size;i++)
+// //         {
+// //             long long number=0;
+// //             string result="";
+// //             int sum=0;
+
+
+// //             int l = queries[i][0];
+// //             int r = queries[i][1];
+
+// //             for(int j=l;j<=r;j++)
+// //             {
+// //                 if(s[j]!='0')
+// //                 {
+// //                     number = (number * 10 + (s[j] - '0')) % MOD;
+// //                 }
+// //                 sum+=(s[j]-'0');
+// //             }
+// //             // long long  val=number*sum;
+// //             // ans.push_back(number * sum);
+// //             long long val = (number % MOD) * (sum % MOD) % MOD;
+// //             ans.push_back((int)val);
+// //         }
+// //     return ans;
+// //     }
+// // };
+
+
+
 // class Solution {
 // public:
 //     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+
 //         const long long MOD = 1e9 + 7;
-//         vector<int>ans;
-//         int size=queries.size();
-//         for(int i=0;i<size;i++)
-//         {
-//             long long number=0;
-//             string result="";
-//             int sum=0;
 
+//         vector<int> pos;
+//         vector<int> digit;
 
-//             int l = queries[i][0];
-//             int r = queries[i][1];
-
-//             for(int j=l;j<=r;j++)
-//             {
-//                 if(s[j]!='0')
-//                 {
-//                     number = (number * 10 + (s[j] - '0')) % MOD;
-//                 }
-//                 sum+=(s[j]-'0');
+//         // Store non-zero digits and their positions
+//         for(int i = 0; i < s.size(); i++) {
+//             if(s[i] != '0') {
+//                 pos.push_back(i);
+//                 digit.push_back(s[i]-'0');
 //             }
-//             // long long  val=number*sum;
-//             // ans.push_back(number * sum);
-//             long long val = (number % MOD) * (sum % MOD) % MOD;
-//             ans.push_back((int)val);
 //         }
-//     return ans;
+
+//         int n = digit.size();
+
+//         // prefix number
+//         vector<long long> pref(n+1,0);
+
+//         // prefix digit sum
+//         vector<long long> sum(n+1,0);
+
+//         vector<long long> pow10(n+1,1);
+
+
+//         for(int i=0;i<n;i++) {
+
+//             pref[i+1] = (pref[i]*10 + digit[i]) % MOD;
+
+//             sum[i+1] = sum[i] + digit[i];
+
+//             pow10[i+1] = (pow10[i]*10)%MOD;
+//         }
+
+
+//         vector<int> ans;
+
+
+//         for(auto &q: queries) {
+
+//             int l=q[0];
+//             int r=q[1];
+
+
+//             // first non zero >= l
+//             int left = lower_bound(pos.begin(),pos.end(),l)-pos.begin();
+
+
+//             // first non zero > r
+//             int right = upper_bound(pos.begin(),pos.end(),r)-pos.begin();
+
+
+//             // no non-zero digit
+//             if(left==right) {
+//                 ans.push_back(0);
+//                 continue;
+//             }
+
+
+//             // number of digits in this range
+//             int len = right-left;
+
+
+//             // extract number
+//             long long x = 
+//             (pref[right] - (pref[left]*pow10[len])%MOD + MOD)%MOD;
+
+
+//             long long digitSum = sum[right]-sum[left];
+
+
+//             long long result = (x * (digitSum%MOD))%MOD;
+
+
+//             ans.push_back(result);
+//         }
+//         return ans;
 //     }
 // };
 
 
-
 class Solution {
 public:
+    int MOD=1e9+7;
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        int n=s.size();
+        vector<int>nonZeroCount(n,0);
+        vector<long long>numberUpTo(n,0);
+        vector<long long >digitSumUpTo(n,0);
+        vector<long long>pow10(n+1,0);  // 10 ^i
 
-        const long long MOD = 1e9 + 7;
+        pow10[0]=1;
+        for(int i=1;i<=n;i++)
+        {
+            pow10[i]=(pow10[i-1]*10)%MOD;
+        }
 
-        vector<int> pos;
-        vector<int> digit;
+        nonZeroCount[0]=(s[0]!='0') ? 1:0;
+        for(int i=1;i<n;i++)
+        {
+            int digit=(s[i]-'0');
+            nonZeroCount[i]=nonZeroCount[i-1]+((digit!=0) ? 1:0);
+        }
 
-        // Store non-zero digits and their positions
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] != '0') {
-                pos.push_back(i);
-                digit.push_back(s[i]-'0');
+        numberUpTo[0]=s[0]-'0';
+        for(int i=1;i<n;i++)
+        {
+            int digit=s[i]-'0';
+            if(digit!=0)
+            {
+                numberUpTo[i]=((numberUpTo[i-1]*10)+digit)%MOD;
+            }
+            else
+            {
+                numberUpTo[i]=numberUpTo[i-1];
             }
         }
 
-        int n = digit.size();
-
-        // prefix number
-        vector<long long> pref(n+1,0);
-
-        // prefix digit sum
-        vector<long long> sum(n+1,0);
-
-        vector<long long> pow10(n+1,1);
-
-
-        for(int i=0;i<n;i++) {
-
-            pref[i+1] = (pref[i]*10 + digit[i]) % MOD;
-
-            sum[i+1] = sum[i] + digit[i];
-
-            pow10[i+1] = (pow10[i]*10)%MOD;
+        digitSumUpTo[0]=s[0]-'0';
+        for(int i=1;i<n;i++)
+        {
+            int digit=s[i]-'0';
+            digitSumUpTo[i]=digitSumUpTo[i-1]+digit;
         }
 
+        int q=queries.size();
+        vector<int>ans(q);
+        for(int i=0;i<q;i++)
+        {
+            int l=queries[i][0];
+            int r=queries[i][1];
+        
 
-        vector<int> ans;
+        long long sum=digitSumUpTo[r]-((l==0)? 0: digitSumUpTo[l-1]);
+        
+        int number=(l==0) ? 0 : numberUpTo[l-1];
+        int k=nonZeroCount[r]-((l==0)? 0 : nonZeroCount[l-1]);
+        long long x = (numberUpTo[r]-(number* pow10[k] %MOD)+MOD)%MOD;
 
-
-        for(auto &q: queries) {
-
-            int l=q[0];
-            int r=q[1];
-
-
-            // first non zero >= l
-            int left = lower_bound(pos.begin(),pos.end(),l)-pos.begin();
-
-
-            // first non zero > r
-            int right = upper_bound(pos.begin(),pos.end(),r)-pos.begin();
-
-
-            // no non-zero digit
-            if(left==right) {
-                ans.push_back(0);
-                continue;
-            }
-
-
-            // number of digits in this range
-            int len = right-left;
-
-
-            // extract number
-            long long x = 
-            (pref[right] - (pref[left]*pow10[len])%MOD + MOD)%MOD;
-
-
-            long long digitSum = sum[right]-sum[left];
-
-
-            long long result = (x * (digitSum%MOD))%MOD;
-
-
-            ans.push_back(result);
+        ans[i]=(int)((x*sum)%MOD);
         }
-
-
         return ans;
     }
 };
