8cd9ebc575a5a839446b23ca0974492ae256b4c0
Manoj Thakur <manoj99065@gmail.com>
2026-08-04 17:09:51 +0530

    Time: 1 ms (33.84%) | Memory: 23.1 MB (68.21%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -1,29 +1,62 @@
+// class Solution {
+// public:
+//     int maxFrequencyElements(vector<int>& nums) {
+//         map<int ,int>mp;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             mp[nums[i]]++;
+//         }
+
+//         int val=INT_MIN;
+        
+
+//         for(auto i:mp)
+//         {
+//             val=max(val,i.second);
+//         }
+
+//         int ans=0; 
+//         for(auto i:mp)
+//         {
+//             if(i.second ==val)
+//             {
+//                 ans=ans+val;
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
-        map<int ,int>mp;
-        for(int i=0;i<nums.size();i++)
-        {
-            mp[nums[i]]++;
-        }
+        //precompute
+map<int ,int >mpp;
 
-        int val=INT_MIN;
+for(int i=0; i<nums.size(); i++){
+    mpp[nums[i]]++;
+}
+
+int maxfreq = 0;
+
+for(auto it : mpp){
+    if(it.second > maxfreq){
+        maxfreq = it.second;
+    }
+}
+
+int ans = 0;
+
+for(auto it : mpp){
+    if(it.second == maxfreq){
+        ans += it.second;
+    }
+}
+
+return ans;
         
-
-        for(auto i:mp)
-        {
-            val=max(val,i.second);
-        }
-
-        int ans=0; 
-        for(auto i:mp)
-        {
-            if(i.second ==val)
-            {
-                ans=ans+val;
-            }
-        }
-
-        return ans;
+        
     }
 };
